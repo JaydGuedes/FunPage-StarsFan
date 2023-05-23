8ccf17fe333daa9353ae51be581cec66b30ffa54
JaydGuedes <jayd.paiva@gmail.com>
2023-05-23 20:44:16 -0300

    comErro

--- js/index.js
+++ js/index.js
@@ -49,14 +49,14 @@ function myApp() {
         _order: 'desc'
     })
         .done((data) => {
-            data.results.forEach((art, i) => {
+            data.results.forEach((art) => {
                 var nId = rastreaId(art.url)
                 listaPer += `
                     <option value="${nId}">${art.name}</option>                   
                 `
             })
             listaPer += '</select>';
-            $('#artList').html(listaPer)
+            $('#').html(listaPer)
 
             //getMostViewed()
             //getLastComments()
@@ -74,7 +74,7 @@ function umPerson(id) {
     $.get(app.apiBaseURL + 'people/' + id + '/')
         .done((data) => {
             person += `
-                <div class="article art-item" data-id="${data.id}">
+                <div class="digited" data-id="${data.id}">
                 <h3>Nome: ${data.name}, 
                 Altura: ${data.height}, 
                 Peso: ${data.mass}, 
@@ -84,20 +84,10 @@ function umPerson(id) {
                 Data de nascimento: ${data.birth_year}, 
                 Sexo: ${data.gender}, 
                 Planeta natal:  ${data.homeworld =! "n/a" ? planet(rastreaId(data.homeworld)) : "n/a"}, 
-                ${data.films}: [
-                "https://swapi.dev/api/films/1/", 
-                "https://swapi.dev/api/films/2/", 
-                "https://swapi.dev/api/films/3/", 
-                "https://swapi.dev/api/films/6/"
-                ], 
-                ${"species"}: [], 
-                ${"vehicles"}: [
-                "https://swapi.dev/api/vehicles/14/", 
-                "https://swapi.dev/api/vehicles/30/"
-                ], 
-                ${"starships"}: [
-                "https://swapi.dev/api/starships/12/", 
-                "https://swapi.dev/api/starships/22/"</h3>
+                Participou dos filmes: ${data.films.length =! 0 ? film(data.films) : "n/a"}, 
+                Espécie: ${data.species.length =! 0 ? especie(data.species) : "n/a"}, 
+                Veículos: ${data.vehicles.length =! 0 ? veiculo(data.vehicles) : "n/a"},
+                Naves: ${data.starships.length =! 0 ? nave(data.starships) : "n/a"} </h3>
                 </div>                    
             `
             $('#artList').html(person)
@@ -122,55 +112,91 @@ function planet(id) {
         })
 
 }
-// base pra uso
-function planet(id) {
+// busca os filmes que participou
+function film(listaFilmes) {
 
-    var planetName = '';
+    var filmList = '';
+    listaFilmes.forEach((art, i) => {    
 
-    $.get(app.apiBaseURL + 'people/' + id + '/')
-        .done((data) => {
-            planetName = data.name
-            data.results.forEach((art) => {
-                person += `
-                    <div class="article art-item" data-id="${art.id}">
-                    <h3>Nome: ${art.name}, 
-            Altura: ${art.height}, 
-            Peso: ${art.mass}, 
-            Cor do cabelo: ${art.hair_color}, 
-            Cor da pele: ${art.skin_color}, 
-            Cor dos olhos: ${art.eye_color}, 
-            Data de nascimento: ${art.birth_year}, 
-            Sexo: ${art.gender}, 
-            "homeworld": "https://swapi.dev/api/planets/1/", 
-            "films": [
-                "https://swapi.dev/api/films/1/", 
-                "https://swapi.dev/api/films/2/", 
-                "https://swapi.dev/api/films/3/", 
-                "https://swapi.dev/api/films/6/"
-            ], 
-            "species": [], 
-            "vehicles": [
-                "https://swapi.dev/api/vehicles/14/", 
-                "https://swapi.dev/api/vehicles/30/"
-            ], 
-            "starships": [
-                "https://swapi.dev/api/starships/12/", 
-                "https://swapi.dev/api/starships/22/"</h3>
-                    </div>                    
-                `
+        $.get(app.apiBaseURL + 'films/' + rastreaId(art) + '/')
+            .done((data) => {
+                if (i == listaFilmes.length){
+                filmList += `${data.title} .`}
+                else{
+                filmList += `${data.title} &`}
+                return filmList                
             })
-            person += '';
-            $('#artList').html(person)
-
-            getMostViewed()
-            getLastComments()
-        })
+            
+    })
         .fail((error) => {
-            $('#artList').html('<p class="center">Oooops! Não encontramos nenhum artigo...</p>')
+            $('#artList').html('<p class="center">Oooops! Não encontramos filmes...</p>')
         })
 
 }
-// base fimm
+// busca espécies
+function especie(listEspecie) {
+
+    var especieList = '';
+    listEspecie.forEach((art, i) => {    
+
+        $.get(app.apiBaseURL + 'species/' + rastreaId(art) + '/')
+            .done((data) => {
+                if (i == listEspecie.length){
+                    especieList += `${data.title} .`}
+                else{
+                    especieList += `${data.title} &`}
+                return especieList                
+            })
+            
+    })
+        .fail((error) => {
+            $('#artList').html('<p class="center">Oooops! Não encontramos a espécie...</p>')
+        })
+
+}
+// busca veiculos
+
+function veiculo(listVeiculos) {
+
+    var veiculoList = '';
+    listVeiculos.forEach((art, i) => {    
+
+        $.get(app.apiBaseURL + 'vehicles/' + rastreaId(art) + '/')
+            .done((data) => {
+                if (i == listVeiculos.length){
+                    veiculoList += `${data.title} .`}
+                else{
+                    veiculoList += `${data.title} &`}
+                return veiculoList                
+            })
+            
+    })
+        .fail((error) => {
+            $('#artList').html('<p class="center">Oooops! Não encontramos veículos...</p>')
+        })
+
+}
+// Busca naves
+function nave(listNaves) {
+
+    var navesList = '';
+    listNaves.forEach((art, i) => {    
+
+        $.get(app.apiBaseURL + 'starships/' + rastreaId(art) + '/')
+            .done((data) => {
+                if (i == listNaves.length){
+                    navesList += `${data.title} .`}
+                else{
+                    navesList += `${data.title} &`}
+                return navesList                
+            })
+            
+    })
+        .fail((error) => {
+            $('#artList').html('<p class="center">Oooops! Não encontramos naves...</p>')
+        })
+
+}
 
 // formata id
 function rastreaId(urrl) {
@@ -187,10 +213,11 @@ function typeWriter(elemento){
     elemento.innerhtml = ''
     textoArray.forEach((letra,i) => {
         if(letra == ',') letra = '<br>'
+        if(letra == '&') letra = '; <br> '
         setTimeout(() => elemento.innerhtml += letra, 75 * i)
     });
 }
-var descricao = document.querySelector('.digited');
+var descricao = document.querySelector('#digited');
 console.log(descricao)
 typeWriter(descricao);
 
